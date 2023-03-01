127c3a55a8a1f4f8d64a2194b1f6fe7982473b91
Antônio Matheus Follone Graça <matheus-follone@follone.com.br>
2023-03-01 20:12:59 -0300

    página de admin
    inclusão do crud de Curso e Aluno

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from escola.models import Aluno,Curso
 # Register your models here.
+
+class Alunos(admin.ModelAdmin):
+    # campos que quero exibir no admin da tabela aluno
+    list_display = ('id','nome','rg','cpf','data_nascimento')
+    # links para alterar alunos pelos campos id e nome
+    list_display_links = ('id','nome')
+    # busca por campo
+    search_fields = ('nome',) # , é obrigatório no final 
+    list_per_page = 20
+
+
+
+# registrando mudanças no admin
+admin.site.register(Aluno,Alunos)
+
+# mudanças envolvendo Cursos
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id','codigo_curso','descricao','nivel')
+    list_display_links =('id','codigo_curso')
+    search_fields = ('codigo_curso',) # , é obrigatório no final 
+
+# registrando mudanças no admin
+admin.site.register(Curso, Cursos)
--- escola/models.py
+++ escola/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Aluno(models.Model):
-    nome = models.CharField(max_length= 30),
-    rg = models.CharField(max_length= 9),
-    cpf = models.CharField(max_length= 11),
+    nome = models.CharField(max_length= 30)
+    rg = models.CharField(max_length= 9)
+    cpf = models.CharField(max_length= 11)
     data_nascimento = models.DateField()
 
     def __str__(self):
@@ -19,10 +19,9 @@ class Curso(models.Model):
         ('B', 'Básico'),
         ('I', 'Intermediário'),
         ('A','Avançado')
-
     )
-    codigo_curso = models.CharField(max_length= 10),
-    descricao = models.CharField(max_length= 100),
+    codigo_curso = models.CharField(max_length= 10)
+    descricao = models.CharField(max_length= 100)
     nivel = models.CharField(max_length=1, choices= NIVEL, blank= False, null= False, default='B')
          
 
