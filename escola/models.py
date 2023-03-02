a08cf29f3f1a47ed25691e54c63a91d6b6b0d19f
Antônio Matheus Follone Graça <matheus-follone@follone.com.br>
2023-03-01 21:08:41 -0300

    Arrumei o erro do insert de alunos
    Era  a tabela que não atualizou do arquivo dbsqlite

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+# classe Aluno
+class Aluno(models.Model):
+    nome = models.CharField(max_length= 30)
+    rg = models.CharField(max_length= 9)
+    cpf = models.CharField(max_length= 11)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+# criando model curso
+
+class Curso(models.Model):
+    # escolhas de nivel do curso
+    NIVEL =(
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A','Avançado')
+    )
+    codigo_curso = models.CharField(max_length= 10)
+    descricao = models.CharField(max_length= 100)
+    nivel = models.CharField(max_length=1, choices= NIVEL, blank= False, null= False, default='B')
+
+
+    def __str__(self) :
+        return self.descricao
