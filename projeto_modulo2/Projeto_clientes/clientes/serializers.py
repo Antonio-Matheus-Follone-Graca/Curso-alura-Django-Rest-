92b94ec00b228586bb78176172e2b251c807e9c0
Antônio Matheus Follone Graça <matheus-follone@follone.com.br>
2023-03-07 21:11:52 -0300

    Validações do projeto 2 do curso de Djando Rest

--- projeto_modulo2/Projeto_clientes/clientes/serializers.py
+++ projeto_modulo2/Projeto_clientes/clientes/serializers.py
@@ -1,44 +1,18 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    # validações por campo
-    def  validate_cpf(self, cpf):
-        # se  o cpf for diferente de 11 números
-        if len(cpf) != 11:
-            # exibindo mensagem de erro
-            raise serializers.ValidationError("O cpf deve ter 11 dígitos")
-        # retornando valor de   cpf 
-        return  cpf
-    
-    def validate_nome(self,nome):
-        # caso só existam número no nome
-        if not nome.isalpha():
-            # exibindo mensagem de erro
-            raise serializers.ValidationError("Não includa números neste campo")
-        # retornando valor de   nome 
-        return  nome
-
-    def validate_rg(self,rg):
-        if len(rg)!= 9:
-            # exibindo mensagem de erro
-            raise serializers.ValidationError("O RG deve ter 9 dígitos")
-        # retornando valor de  rg 
-        return  rg
-    
-    def validate_celular(self,celular):
-        if len(celular) < 11:
-            # exibindo mensagem de erro
-            raise serializers.ValidationError("O celular deve ter 11 dígitos")
-        # retornando valor de  celular 
-        return  celular
-
-
-
-    
-
-    
-    
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"Número de CPF inválido"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O número de celular deve seguir este modelo: 11 91234-1234 (respeitando os espaços e traço)"})
+        return data
