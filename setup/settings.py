04281352d273bf3ede9bdadc2e0afafca45ce04e
Antônio Matheus Follone Graça <matheus-follone@follone.com.br>
2023-03-09 23:09:32 -0300

    Limitação de número  de requisições por tipo de usuário
    Limitação de número  de requisições por tipo de usuário

--- setup/settings.py
+++ setup/settings.py
@@ -136,7 +136,16 @@ REST_FRAMEWORK = {
      # adicionando autenticação padrão ao rest framework
      'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.BasicAuthentication'
-     ]
+     ],
+     # limitação de números de requisições da api por tipo de usuário
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/day',
+        'user': '100/day'
+    }
 }
 
 # Default primary key field type
